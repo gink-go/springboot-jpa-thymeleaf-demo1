1ab777de489d73d38e03166604267d045a498196
gink-go <59662327+gink-go@users.noreply.github.com>
2020-01-09 16:41:56 +0800

    Update SpringbootJpaThymeleafDemo1Application.java

--- src/main/java/com/example/SpringbootJpaThymeleafDemo1Application.java
+++ src/main/java/com/example/SpringbootJpaThymeleafDemo1Application.java
@@ -2,7 +2,7 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//**********
 @SpringBootApplication
 public class SpringbootJpaThymeleafDemo1Application {
     /**
